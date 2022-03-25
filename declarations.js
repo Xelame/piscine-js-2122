514c1e20b9031e8ddc5ac64d5dd5fe9902a79094
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 08:48:26 +0100

    try to not freeze arr

--- declarations.js
+++ declarations.js
@@ -1,6 +1,6 @@
 const escapeStr = "\'\`\"\\\/"
 const arr = [4, '2']
-Object.freeze(arr)
+
 const obj = {str: 'String', num: 1, bool: true, undef: undefined}
 Object.freeze(obj)
 const nested = {arr: [4, '2'], obj: {str: 'String', num: 1, bool: true, undef: undefined}}
