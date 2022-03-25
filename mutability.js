98fd7b5e52165a791858d72589e715b45b0b3f4a
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 09:06:54 +0100

    push 1 mutability

---
+++ mutability.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+const clone1 = Object.assign({}, person)
+const clone2 = Object.assign({}, person)
+const samePerson = Object.assign({}, person)
+
+person.age ++
+person.country = 'FR'
