6f9c13b5e43c2ffe8320b1e8822ed4a63b3a368e
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 15:05:27 +0100

    wouaf wouaf

--- physics.js
+++ physics.js
@@ -1,9 +1,9 @@
 function getAcceleration (Object) {
-    if (Object.f != undefined || Object.m != undefined) {
+    if (!(Object.f == undefined || Object.m == undefined)) {
         return Object.f / Object.m;
-    } else if (Object.Δv != undefined || Object.Δt != undefined) {
+    } else if (!(Object.Δv == undefined || Object.Δt == undefined)) {
         return Object.Δv / Object.Δt;
-    } else if (Object.d != undefined || Object.t != undefined) {
+    } else if (!(Object.d == undefined || Object.t == undefined)) {
         return 2*Object.d / Object.t**2;
     } else {
         return "impossible";
