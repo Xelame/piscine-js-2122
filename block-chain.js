8edf62115b65987f9e8703b167c8edaf9cd10112
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 16:52:29 +0100

    wouaf wouaf

--- block-chain.js
+++ block-chain.js
@@ -1,7 +1,7 @@
 function blockChain (data, prev = { index: 0, hash: '0' }) {
     return {
         index: prev.index + 1,
-        hash : hashCode(`${index}${prev.hash}${JSON.stringify(data)}`),
+        hash : hashCode(`${prev.index + 1}${prev.hash}${JSON.stringify(data)}`),
         prev : prev,
         data : data,
     }; 
