7d9ad918cb5ad6fb14a774f979cf3455dc532de0
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 08:40:50 +0100

    push 1 declaration

---
+++ declarations.js
@@ -0,0 +1,7 @@
+const escapeStr = "\'\`\"\\"
+const arr = [4, '2']
+Object.freeze(arr)
+const obj = {str: 'String', num: 1, bool: true, undef: undefined}
+Object.freeze(obj)
+const nested = {arr: [4, '2'], obj: {str: 'String', num: 1, bool: true, undef: undefined}}
+Object.freeze(nested)
