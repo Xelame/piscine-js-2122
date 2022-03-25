a237b672c3d0f8c86768d543eff1b9ef406e4bf8
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 09:12:32 +0100

    sameperson clone better

--- mutability.js
+++ mutability.js
@@ -1,6 +1,6 @@
 const clone1 = Object.assign({}, person)
 const clone2 = Object.assign({}, person)
-const samePerson = Object.assign({}, person)
+const samePerson = person
 
 person.age ++
 person.country = 'FR'
