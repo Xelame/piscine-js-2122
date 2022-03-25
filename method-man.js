1cd0070ed368cb90a61b28912ac3e89e153a0b4b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 11:23:56 +0100

    wouf wouf 1

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words (strings) {
+    return strings.split(' ')
+}
+
+function sentence (array) {
+    return array.join(' ')
+}
+
+function yell (strings) {
+    return strings.toUpperCase();
+}
+
+function whisper (strings) {
+    return '*' + strings.toLowerCase() + '*';
+}
+
+function capitalize (strings) {
+    return strings.charAt(0).toUpperCase() + strings.slice(1);
+}
