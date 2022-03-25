9c72f5e64783ff381cdf4d42ba23ed382df03827
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-25 08:52:21 +0100

    freeze all

--- declarations.js
+++ declarations.js
@@ -3,5 +3,4 @@ const arr = [4, '2']
 Object.freeze(arr)
 const obj = {str: 'String', num: 1, bool: true, undef: undefined}
 Object.freeze(obj)
-const nested = {arr: [4, undefined, '2'], obj: {str: 'String', num: 1, bool: true}}
-Object.freeze(nested)
+const nested = {arr: Object.freeze([4, undefined, '2']), obj: Object.freeze({str: 'String', num: 1, bool: true})}
