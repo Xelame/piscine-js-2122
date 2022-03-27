3825ce694c487f74f2f546cf67e083e870aad016
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 00:35:48 +0200

    miaou

--- cut-corners.js
+++ cut-corners.js
@@ -1,58 +1,60 @@
 function round(float) {
     let num = 0;
     if (float > 0) {
-        while (num < float-0.5) {
+        while (num < float-1) {
             num++
         }
+        if (num+0,5 < float) {
+            return num+1;
+        }
     } else {
-        while (num > float+0.5) {
+        while (num > float+1) {
             num--
+        }
+        if (num-0,5 > float) {
+            return num-1;
         }
     }
     return num;
 }
 
 function ceil(float) {
-    let num = 0;
-    if (float > 0) {
-        while (num < float) {
-            num++
+    let num = round(float);
+    if (num > 0) {
+        if (num < float) {
+            return num+1;
         }
-        return num;
     } else {
-        while (num > float) {
-            num--
+        if (num > float) {
+            return num-1;
         }
-        return num+1;
     }
+    return num;
 }
 
 function floor(float) {
-    let num = 0;
-    if (float > 0) {
-        while (num < float) {
-            num++
+    let num = round(float);
+    if (num > 0) {
+        if (num > float) {
+            return num-1;
         }
-        return num-1;
     } else {
-        while (num > float) {
-            num--
+        if (num < float) {
+            return num-1;
         }
-        return num;
     }
+    return num;
 }
-
 function trunc(float) {
     let num = 0;
     if (float > 0) {
-        while (num < float) {
+        while (num < float-1) {
             num++
         }
-        return num-1;
     } else {
-        while (num > float) {
+        while (num > float+1) {
             num--
-        }
-        return num+1;
+        };
     }
+    return num;
 }
