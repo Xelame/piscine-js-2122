e70a813a7ae2e1b0c2bef72f06b90bba58a34e69
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 01:10:15 +0200

    miaou

--- cut-corners.js
+++ cut-corners.js
@@ -31,12 +31,12 @@ function ceil(float) {
 function trunc(float) {
     let num = 0;
     if (float > 1) {
-        if (num < float-1) {
+        while (num < float-1) {
             num++
             console.log(num);
         }
     } else if (float < 1) {
-        if (num > float+1) {
+        while (num > float+1) {
             num--
             console.log(num)
         }
