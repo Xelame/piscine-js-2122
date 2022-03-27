8c8b44dca1524587d7e00397ae26131091a079b7
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-27 11:28:57 +0200

    miaou

--- elementary.js
+++ elementary.js
@@ -1,9 +1,57 @@
-function multiply (a, b) {
-  var result = 0;
-  var i = 0;
-  while (i < b) {
-    result += a;
-    i++;
-  }
-  return result;
+function multiply(a, b) {
+	var isNegative = false;
+	var result = 0;
+	if (b < 0) {
+		isNegative = true;
+		b = -b;
+	}
+	for (var i = 0; i < b; i++) {
+		result += a;
+	}
+	if (isNegative) {
+		result = -result;
+	}
+	return result;
+}
+
+function divide(a, b) {
+	var isNegative = false;
+	var result = 0;
+	if (a < 0) {
+		isNegative = true;
+		a = -a;
+	}
+	if (b < 0) {
+		isNegative = !isNegative;
+		b = -b;
+	}
+	while (a >= b) {
+		a -= b;
+		result++;
+	}
+	if (isNegative) {
+		result = -result;
+	}
+	return result;
+}
+
+function modulo(a, b) {
+	var isNegative = false;
+	var result = 0;
+	if (a < 0) {
+		isNegative = true;
+		a = -a;
+	}
+	if (b < 0) {
+		isNegative = !isNegative;
+		b = -b;
+	}
+	while (a >= b) {
+		a -= b;
+		result++;
+	}
+	if (isNegative) {
+		result = -result;
+	}
+	return result;
 }
