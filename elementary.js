cbcd7f100f0a6526c35d184c89022f95073c8205
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-27 11:21:37 +0200

    miaou

--- elementary.js
+++ elementary.js
@@ -1,7 +1,9 @@
 function multiply (a, b) {
-    var sum = 0;
-    for (var i = 0; i < b; i++) {
-        sum+=a;
-    }
-    return sum;
+  var result = 0;
+  var i = 0;
+  while (i < b) {
+    result += a;
+    i++;
+  }
+  return result;
 }
