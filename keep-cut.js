2fa5ac91983d6ee00cdbcea717ba7179ada1681c
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-27 12:44:48 +0200

    miaou

---
+++ keep-cut.js
@@ -0,0 +1,23 @@
+function cutFirst (str) {
+  return str.slice(2);
+}
+
+function cutLast (str) {
+  return str.slice(0, -2);
+}
+
+function cutFirstLast (str) {
+  return cutLast(cutFirst(str));
+}
+
+function keepFirst (str) {
+  return str.slice(0, 2);
+}
+
+function keepLast (str) {
+  return str.slice(-2);
+}
+
+function keepFirstLast (str) {
+  return keepFirst(keepLast(str));
+}
