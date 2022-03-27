f243a234aba3c564577783e7b85198c8344e1225
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-27 12:36:58 +0200

    miaou

--- slicer.js
+++ slicer.js
@@ -3,6 +3,9 @@ function slice(array, begin = 0, end = array.length) {
     if (begin < 0) {
         begin = array.length + begin;
     }
+    if (end < 0) {
+        end = array.length + end;
+    }
     for (var i = begin; i < end; i++) {
         result.push(array[i]);
     }
