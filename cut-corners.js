9f6f805ddf64b89d286d7872860ed9847fcefb44
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-27 13:11:43 +0200

    miaou

---
+++ cut-corners.js
@@ -0,0 +1,43 @@
+function round(float) {
+    let num = 0;
+    if (float > 0) {
+        while (num < float*2) {
+            num++
+        }
+    } else {
+        while (num > float*2) {
+            num--
+        }
+    }
+    return num/2;
+}
+
+function ceil(float) {
+    let num = 0;
+    if (float > 0) {
+        while (num < float) {
+            num++
+        }
+        return num+1;
+    } else {
+        while (num > float) {
+            num--
+        }
+        return num;
+    }
+}
+
+function floor(float) {
+    let num = 0;
+    if (float > 0) {
+        while (num < float) {
+            num++
+        }
+        return num;
+    } else {
+        while (num > float) {
+            num--
+        }
+        return num-1;
+    }
+}
