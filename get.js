4be7577e9a19148c4b114e1f271e9e3da8b882d4
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 10:20:44 +0200

    wouaf wouaf

--- get.js
+++ get.js
@@ -1,14 +1,10 @@
-function get(src, path){
-    path.split('.').forEach(function(key){
+function get(src, path) {
+    path.split('.').forEach(function(key) {
         if (src[key] === undefined || src[key] === null) {
             return undefined;
         } else {
             src = src[key];
         }
     });
-    return src;
+    return src[key];
 }
-
-const src = { nested: { key: 'peekaboo' } }
-const path = 'nested.key';
-console.log(get(src, path));
