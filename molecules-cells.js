2fc230531f38eeb81fac6ecc09084b9014d6e6f9
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 09:51:45 +0200

    wouaf wouaf

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,41 +1,43 @@
-function RNA (rna) {
+function RNA(rna) {
     let dna = '';
     for (var i = 0; i < rna.length; i++) {
         switch (rna[i]) {
-            case 'A':
-                dna += 'T';
-                break
-            case 'C':
-                dna += 'G';
+            case 'T':
+                dna += 'A';
                 break
             case 'G':
                 dna += 'C';
                 break
-            case 'U':
-                dna += 'A'; 
+            case 'C':
+                dna += 'G';
+                break
+            case 'A':
+                dna += 'U';
                 break
         }
     }
     return dna;
 }
 
-function DNA (dna) {
+function DNA(dna) {
     let rna = '';
     for (var i = 0; i < dna.length; i++) {
         switch (dna[i]) {
-            case 'A':
-                rna += 'U';
-                break
-            case 'C':
-                rna += 'G';
+            case 'U':
+                rna += 'A';
                 break
             case 'G':
                 rna += 'C';
                 break
-            case 'T':
-                rna += 'A';
+            case 'C':
+                rna += 'G';
+                break
+            case 'A':
+                rna += 'U';
                 break
         }
     }
-    return rna; 
+    return rna;
 }
+
+console.log(RNA('TACG'));
