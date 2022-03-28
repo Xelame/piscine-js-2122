30ff76fcb0203bc2ae30ab931f933576024cc686
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 01:37:34 +0200

    miaou

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,5 +1,13 @@
-function vowel(str = '') {
+function vowels(str) {
+    let regex = new RegExp(/.\s\d\b/, 'g');
+    let result = str.match(regex);
+    return result == null ? [] : result;
+}
+
+function vowelDots(str = '') {
     let regex = new RegExp(/[aeiou]/, 'g'); // /[aeiou]/g is a global regex that matches all vowels in the string.
     let result = str.replace(regex, '$&.'); // $& is the matched string
     return result;
 }
+
+console.log(vowelDots('hello')); // he.llo.
