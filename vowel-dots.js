12611ced8cb051ec091be320ab9626f52d3d24a1
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 01:45:39 +0200

    miaou

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,12 +1,7 @@
-function vowels(str) {
-    let regex = new RegExp(/.\s\d\b/, 'g');
-    let result = str.match(regex);
-    return result == null ? [] : result;
-}
+let vowels = new RegExp(/[aeiou]/, 'g'); // /[aeiou]/g is a global regex that matches all vowels in the string.
 
 function vowelDots(str = '') {
-    let regex = new RegExp(/[aeiou]/, 'g'); // /[aeiou]/g is a global regex that matches all vowels in the string.
-    let result = str.replace(regex, '$&.'); // $& is the matched string
+    let result = str.replace(vowels, '$&.'); // $& is the matched string
     return result;
 }
 
