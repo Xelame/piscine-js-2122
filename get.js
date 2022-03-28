877fb7b6c5bfb917283f825a5736982f47374abd
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 10:03:32 +0200

    wouaf wouaf

--- get.js
+++ get.js
@@ -1,6 +1,10 @@
 function get(src, path){
     path.split('.').forEach(function(key){
-        src = src[key];
+        if (src[key] === undefined || src[key] === null) {
+            src = undefined;
+        } else {
+            src = src[key];
+        }
     });
     return src;
 }
