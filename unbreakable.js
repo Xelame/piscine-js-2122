fcf46474ea0590bd0db7ef6dd110349911aebfb9
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 09:14:42 +0200

    wouaf wouaf

--- unbreakable.js
+++ unbreakable.js
@@ -10,3 +10,14 @@ function split(string, separator) {
     result.push(string)
     return result;
 }
+
+function join(array, separator) {
+    let result = '';
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+        if (i !== array.length-1) {
+            result += separator;
+        }
+    }
+    return result;
+}
