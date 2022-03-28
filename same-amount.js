1cb43da69cc71dd4421ef7f3436bf14b0dde74d4
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 01:51:48 +0200

    miaou

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,8 @@
 function sameAmount (str, regex1, regex2) {
     let result1 = str.match(regex1);
     let result2 = str.match(regex2);
+    if (result1 == null || result2 == null) {
+        return false;
+    }
     return result1.length === result2.length;
 }
