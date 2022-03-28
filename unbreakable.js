6fd501ae4cdf3dc6a6b88cb8a4c77a71baedf02a
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 08:59:07 +0200

    wouaf wouaf

--- unbreakable.js
+++ unbreakable.js
@@ -1,9 +1,8 @@
 function split(string, separator) {
     let result = []
+    string = string.replace(separator, '');
     for (let i = 0; i < string.length; i++) {
-        if (!(string[i] === separator)) {
-            result.push(string[i])
-        }
+        result.push(string[i])
     }
     return result
 }
