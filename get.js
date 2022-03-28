cb4bf7e6b4cd4d928151c87dc3fd65aae0596afb
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 10:41:16 +0200

    wouaf wouaf

--- get.js
+++ get.js
@@ -1,6 +1,20 @@
-function get(src, path) {
-    src.split('.').forEach(function(key) {
-        src = src[key];
-    });
-    return src;
+function get(src, path){
+    if (src.includes('.')){
+        path.split('.').forEach(function(key){
+            if (src[key] !== undefined){
+                src = src[key];
+            } else {
+                return undefined;
+        }   });
+    } else {
+        if (!(src[path] === undefined)) {
+            return src[path];
+        } else {
+            return undefined;
+        }
+    }
 }
+
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key';
+console.log();
