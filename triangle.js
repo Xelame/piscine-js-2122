d8a592473aa349ad56356eb40e3cadaafb5126af
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 11:22:47 +0200

    wouaf wouaf

--- triangle.js
+++ triangle.js
@@ -1,8 +1,9 @@
 function triangle (str, num) {
   var output = '';
+  var flag = str;
   for (var i = 0; i < num; i++) {
     output += str + '\n';
-    str += str
+    str += flag
   }
   return output;
 }
