738b03904923df9c21f60646b23d9c3647721266
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 19:04:24 +0200

    miaou

--- pyramid.js
+++ pyramid.js
@@ -1,9 +1,9 @@
 function pyramid (string, number) {
-    let numberOfSpaces = number - 1;
+    let numberOfSpaces = number - string.length;
     let result = "";
     for (var i = 1; numberOfSpaces >= 0; i+=2) {
         result += " ".repeat(numberOfSpaces) + string.repeat(i) + "\n";
-        numberOfSpaces--
+        numberOfSpaces-= string.length;
     }
     return result.slice(0, -1);
 }
