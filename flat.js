532f43bf20bf589dd5b278be40741743e1af681d
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 19:10:56 +0200

    miaou

--- flat.js
+++ flat.js
@@ -1,5 +1,14 @@
-function flat(arr) {
-  return arr.reduce(function(acc, curr) {
-    return acc.concat(curr);
-  }, []);
+function flat(arr, depth = 1) {
+    if (depth === 0) {
+        return arr;
+    }
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(flat(arr[i], depth-1));
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 }
