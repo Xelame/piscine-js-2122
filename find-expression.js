521e813f27902cd36ba4d807df601bd2ae4872e5
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-28 19:17:26 +0200

    miaou

--- find-expression.js
+++ find-expression.js
@@ -2,12 +2,12 @@ const add4 = '+4'
 const mul2 = '*2'
 
 function findExpression(number) {
-    let result = '1 ';
+    let result = '1';
     for (let i = 1; i <= number; i++) {
         if (i % 4 === 0) {
-            result += add4;
+            result += " " + add4;
         } else if (i % 2 === 0) {
-            result += mul2;
+            result += " " + mul2;
         }
     }
     return result;
