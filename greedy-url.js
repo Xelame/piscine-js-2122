4d2772b4157c733d955b931325563bde222f7239
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 10:57:31 +0200

    wouaf wouaf

--- greedy-url.js
+++ greedy-url.js
@@ -1,3 +1,3 @@
 function getURL (url) {
-    return url.match(/(https|http):\/\/(\w*\.\w*\.|\w*\.)[a-zA-Z\/0-9=&?_]*/g) == null ? [] : url.match(/(https|http):\/\/(\w*\.\w*\.|\w*\.)[a-zA-Z\/0-9=&?_]*/g);
+    return url.match(/(https|http):\/\/(\w*\.\w*\.|\w*\.)[a-zA-Z\/0-9=&?_@]*/g) == null ? [] : url.match(/(https|http):\/\/(\w*\.\w*\.|\w*\.)[a-zA-Z\/0-9=&?_@]*/g);
 }
