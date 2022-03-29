89693bb00370a5a2c5705290e3f8b260bda6adfb
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 13:41:44 +0200

    miaou

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,14 @@
+const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
+ ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
+
 function sameAmount (str, regex1, regex2) {
-    let result1 = str.match(regex1);
-    let result2 = str.match(regex2);
+    let result1 = str.match(regex1, 'g');
+    let result2 = str.match(regex2, 'g');
     if (result1 == null || result2 == null) {
         return false;
     }
-    return result1.length === result2.length;
+    console.log(result1.length, result2.length);
+    return result1.length == result2.length;
 }
+
+sameAmount(data, /q /, /qqqqqqq/)
