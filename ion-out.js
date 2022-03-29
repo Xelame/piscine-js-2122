2dd40f55e8e614d67f1288d65283d680bbb2614c
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 10:17:53 +0200

    wouaf wouaf

--- ion-out.js
+++ ion-out.js
@@ -1,3 +1,3 @@
 function ionOut (string) {
-    return string.match(RegExp(/\w*t(?=ion)/g)) == null ? [] : string.replace(RegExp(/\w*t(?=ion)/g), '');
+    return string.match(RegExp(/\w*t(?=ion)/g)) == null ? [] : string.match(RegExp(/\w*t(?=ion)/g), '');
 }
