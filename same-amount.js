d4eba6c4f0ef80c54a12f7d443f34ca41f8ececc
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-29 09:46:24 +0200

    wouaf wouaf

--- same-amount.js
+++ same-amount.js
@@ -1,15 +1,16 @@
 const data = `qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw
 ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd`
 
-function sameAmount (str, regex1, regex2) {
+function sameAmount(str, regex1, regex2) {
     let regex1g = new RegExp(regex1, 'g');
     let regex2g = new RegExp(regex2, 'g');
-    let result1 = regex1g.exec(str);
-    let result2 = regex2g.exec(str);
+    let result1 = str.match(regex1g);
+    let result2 = str.match(regex2g);
     if (result1 == null || result2 == null) {
         return false;
     }
+    console.log(result1.length, result2.length);
     return result1.length === result2.length;
 }
 
-console.log(data, /q /, /qqqqqqq/)
+console.log(sameAmount(data, /q /, /qqqqqqq/))
