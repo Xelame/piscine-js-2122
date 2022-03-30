6727ec7203ea785056f0318bd62d70b6ce0423d0
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-30 12:49:30 +0200

    miaou

--- greedy-url.js
+++ greedy-url.js
@@ -1,4 +1,4 @@
 function getURL (url) {
-    let regex = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g
+    let regex = /(https|http):\/\/([\w_-]+|[\w_-]+\.[\w_-]+|[\w_-]+\.[\w_-]+\.[\w_-]+)([\[\]\w.,@?^=%&:\/~+#-]*[\[\]\w@?^=%&\/~+#-])/g
     return url.match(regex) == null ? [] : url.match(regex);
 }
