c2d1f6ebc281c924ad9a25c4d85cdfadc009bd90
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-30 16:54:12 +0200

    miaou

---
+++ group-price.js
@@ -0,0 +1,12 @@
+function groupPrice (string) {
+    let prices = []
+    let regex = /[a-zA-Z\$]+([0-9]+)\.([0-9]+)/
+    while (string.test(regex)) {
+        let result = string.match(regex);
+        string.replace(regex, '');
+        prices.push([result[0], result[1], result[2]]);
+    }
+    return prices
+}
+
+console.log(groupPrice('The price of the cereals is $4.00.')); // [['$4.00.', '4', '00']]
