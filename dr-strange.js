1c4506e78a7f0bcebc59425e497d4b623c04de9b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-31 11:35:17 +0200

    wouaf wouaf

--- dr-strange.js
+++ dr-strange.js
@@ -1,13 +1,30 @@
 const newWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 
-function addWeek (date) {
-    console.log(date.getDay(), date.getMonth(), date.getFullYear());
-    let day = date.getDate()%14;
-    if (day > 7) {
-        return "second" + newWeek[date.getDay()];
-    }
-    return newWeek[day-1];
-}
 
-console.log(addWeek(new Date('2001-11-07')));
+
+function addWeek(date){
+    let day0 = new Date('0001-01-01')
+    function diffDates(day_one, day_two) {
+        return ((day_one-day_two) / (60 * 60 * 24 * 1000))/7
+    }
+    //Получем день недели
+    function getWeekDay(date) {
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return days[date.getDay()];
+    }
+    let diff = diffDates(date,day0)%2
+    if(diff%2===0||diff<1){
+        return getWeekDay(date)
+    }
+    else {
+        let day = getWeekDay(date)
+        return 'second'+day
+    }
+}
+function timeTravel({date,hour,minute,second}){
+   date.setHours(hour)
+   date.setMinutes(minute)
+   date.setSeconds(second)
+   return date
+}
