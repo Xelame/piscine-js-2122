57d546183a99b4fda6961c10c91533e487b9d8eb
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 00:38:11 +0200

    miaou

--- get-some-time.js
+++ get-some-time.js
@@ -3,7 +3,7 @@ function firstDayWeek(week, year) {
     var day = date.getDay();
     var result  = new Date(year, 0, (week * 7) + 1 - day);
     var weekInString = `${result.getMonth() + 1}`;
-    if (weekInString.length) {
+    if (weekInString.length == 1) {
         weekInString = `0${weekInString}`;
     }
     if (week == 1) {
