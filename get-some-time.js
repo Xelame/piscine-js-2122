e556bedd11f72266055cd55d8e244bfdfa84f6d7
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 00:31:52 +0200

    miaou

--- get-some-time.js
+++ get-some-time.js
@@ -3,7 +3,7 @@ function firstDayWeek(week, year) {
     var day = date.getDay();
     var result  = new Date(year, 0, (week * 7) + 1 - day);
     if (week == 1) {
-        return `$01-${result.getMonth() + 1}-${result.getFullYear()}`;
+        return `01-${result.getMonth() + 1}-${result.getFullYear()}`;
     }
     return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
 }
