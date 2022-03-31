27298a2bd8e7efcd1b072ff4e53e55b665285426
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-03-31 10:19:53 +0200

    wouaf wouaf

--- dr-strange.js
+++ dr-strange.js
@@ -1,3 +1,7 @@
+const newWeek = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, secondMonday, secondTuesday, secondWednesday, secondThursday, secondFriday, secondSaturday, secondSunday]
+
+
 function addWeek (date = new Date('0001-01-01')) {
-  return date.getDate();
+    let day = date.getDay()*date.getMonth()*date.getFullYear()%14;
+    return newWeek[day];
 }
