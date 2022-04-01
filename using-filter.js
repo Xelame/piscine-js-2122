7470354e75e15adad737bf99b53fecae10a3cbd4
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 18:48:36 +0200

    wouaf wouaf

--- using-filter.js
+++ using-filter.js
@@ -28,5 +28,10 @@ function filter1DistinctVowel(array) {
     })
 }
 
-
+function multiFilter(array, fs) {
+    return array.filter(state => state.capital.lenght >= 8)
+    .filter(state => !(/^[AEOIU]/.test(state.name)))
+    .filter(state => state.tag.match(/[AEOIU]/g).length >= 1)
+    .filter(state => state.region != South)
+} 
 
