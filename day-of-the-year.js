d81c449754dbd67b0f397fb932b041abe3e73b9e
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 10:31:25 +0200

    wouaf wouaf

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,6 @@
 function dayOfTheYear(date) {
     let count = 0;
-    for (let i = 1; i < date.getMonth(); i++) {
+    for (let i = 1; i <= date.getMonth(); i++) {
         count += new Date(date.getFullYear(), i, 0).getDate();
     }
     count += date.getDate();
