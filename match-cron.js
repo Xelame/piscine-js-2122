18c065a7dd1a5677fd820a217b85951389fb716b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 10:05:17 +0200

    wouaf wouaf

--- match-cron.js
+++ match-cron.js
@@ -1,30 +1,41 @@
-function matchCron(cron, date) {
-    let match = cron.split(' ');
-    let minute = match[0];
-    let hour = match[1];
-    let dayOfMonth = match[2];
-    let month = match[3];
-    let dayOfWeek = match[4];
-    let year = match[5];
-    let isValid = true;
-    if (!(minute === '*' || minute === date.getMinutes())) {
-        isValid = false;
+function matchCron (cron,date){
+    let arr = cron.split(' ')
+    let minute = arr[0]
+    let hour = arr[1]
+    let dayOfMonth = arr[2]
+    let month = arr[3]
+    let dayWeek = arr[4]
+    // console.log(date.getDay()+1)
+    let flag = false
+    if(minute === '*'|| minute == date.getMinutes()){
+        flag = true
+    }else{
+        return false
     }
-    if (!(hour === '*' || hour === date.getHours())) {
-        isValid = false;
+    if(hour === '*'|| hour == date.getHours()){
+        flag = true
+    }else{
+        return false
     }
-    if (!(dayOfMonth === '*' || dayOfMonth === date.getDate())) {
-        isValid = false;
+    if(dayOfMonth === '*'|| dayOfMonth == date.getDate()){
+        flag = true
+    }else{
+        return false
     }
-    if (!(month === '*' || month === (date.getMonth() + 1))) {
-        isValid = false;
+    if(month === '*'|| month == date.getMonth()+1){
+        flag = true
+    }else{
+        return false
     }
-    if (!(dayOfWeek === '*' || (dayOfWeek === 7 && date.getDay === 0) || (date.getDay !== 0 && Number(dayOfWeek) === date.getDay()))) {
-        isValid = false;
+    if(dayWeek === '*'){
+        flag = true
+    }else if(dayWeek === 7 && date.getDay === 0){
+        flag = true
+    }else if(date.getDay !== 0 && Number(dayWeek) === date.getDay()){
+        flag = true
+    }else{
+        // console.log(date.getDay(),'-',dayWeek)
+        return false
     }
-    if (!(year === '*' || year === date.getFullYear())) {
-        isValid = false;
-    }
-    return isValid;
-
+    return flag
 }
