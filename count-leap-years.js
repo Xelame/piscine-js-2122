8eb72a424628610f06bb1858317d3d6ad0aff630
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 10:13:17 +0200

    wouaf wouaf

--- count-leap-years.js
+++ count-leap-years.js
@@ -3,6 +3,9 @@ function isLeapYear(date) {
   }
 
 function countLeapYears(date) {
+    let years = date.getFullYear()
+    if (date.getFullYear() < 1900) {
+        years -= 1900
     for (let i = 1; i <= date.getFullYear(); i++) {
         if (isLeapYear(new Date(i, 1, 1))) {
             count++;
