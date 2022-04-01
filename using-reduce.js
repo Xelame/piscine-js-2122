d021428ea9d7bd5a5746736ddaf5b054cfbdf419
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 19:17:19 +0200

    wouaf wouaf

--- using-reduce.js
+++ using-reduce.js
@@ -1,3 +1,7 @@
 function adder (array, initial = 0) {
     return array.reduce((previous, current) => previous + current, initial)
 }
+
+function sumOrMul (array, initial = 0) {
+    return array.reduce((previous, current) => current%2 ? previous + current : previous * current, initial)
+}
