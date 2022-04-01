54b93eab31165552d85cf9660560da49aadbb451
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 09:16:06 +0200

    wouaf wouaf

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,3 +1,11 @@
 function isFriday(date) {
   return date.getDay() === 5;
 }
+
+function isWeekend (date) {
+  return date.getDay() === 0 || date.getDay() === 6;
+}
+
+function isLeapYear(date) {
+  return date.getFullYear() % 4 === 0 || date.getFullYear() % 400 === 0 && date.getFullYear() % 100 !== 0;
+}
