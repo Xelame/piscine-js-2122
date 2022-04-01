3a413859f32c3871cea7162bb0d3214fb892365f
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-01 08:37:50 +0200

    wouaf wouaf

--- get-some-time.js
+++ get-some-time.js
@@ -1,7 +1,7 @@
 function firstDayWeek(week, year) {
     var date = new Date(year,0);
     
-    var result  = new Date(year, 0, ((week-1) * 7)+1 - date.getDay()));  
+    var result  = new Date(year, 0, ((week-1) * 7)+1 - date.getDay());  
     var dayInString = `${result.getDate()}`;
     var weekInString = `${result.getMonth() + 1}`;
     if (weekInString.length == 1) {
