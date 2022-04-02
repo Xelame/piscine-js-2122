835d06560ef51af4e84642c578e17098cd8c8ae3
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-02 16:23:05 +0200

    wouaf wouaf

--- filter.js
+++ filter.js
@@ -7,3 +7,25 @@ function filter(array, func) {
     }
     return newArray
 }
+
+function reject(array, func) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (!func(array[i], i, array)) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+
+function partition(array, func) {
+    let newArray = [[], []]
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i], i, array)) {
+            newArray[0].push(array[i])
+        } else {
+            newArray[1].push(array[i])
+        }
+    }
+    return newArray
+}
