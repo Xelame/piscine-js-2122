a60d6c5e84369da998b09e7239363318b5b4ee68
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-02 16:14:43 +0200

    wouaf wouaf

--- filter.js
+++ filter.js
@@ -1,1 +1,9 @@
-function filter()
+function filter(array, func) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i], i, array)) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
