d7fd13284d5834d40b6570ef9ad958f1c24aad36
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-02 14:19:48 +0200

    wouaf wouaf

--- mapper.js
+++ mapper.js
@@ -1,4 +1,4 @@
-function map(array, func, init=0) {
+function map(array, func) {
     const newArray = []
     for (let i = 0; i < array.length; i++) {
         newArray.push(func(array[i]))
@@ -6,7 +6,7 @@ function map(array, func, init=0) {
     return newArray
 }
 
-function flatMap (array, func, init=0) {
+function flatMap (array, func) {
     const newArray = []
     for (let i = 0; i < array.length; i++) {
         newArray.push(...func(array[i]))
