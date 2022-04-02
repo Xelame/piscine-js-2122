e82af4572e34a82c1635d7fb6691cc7b324b5d77
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-02 18:49:36 +0200

    wouaf wouaf

--- reduce.js
+++ reduce.js
@@ -1,13 +1,13 @@
 function fold (array, func, accumulator) {
     for (let i = 0; i < array.length; i++) {
-        accumulator += func(accumulator, array[i], i, array)
+        accumulator = func(accumulator, array[i], i, array)
     }
     return accumulator
 }
 
 function foldRight (array, func, accumulator) {
     for (let i = array.length - 1; i >= 0; i--) {
-        accumulator += func(accumulator, array[i], i, array)
+        accumulator = func(accumulator, array[i], i, array)
     }
     return accumulator
 }
