c81c97c51764dea0ca887ca16f95ccdd5e8c925b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-02 18:56:16 +0200

    wouaf wouaf

---
+++ quantifiers.js
@@ -0,0 +1,17 @@
+function every (array, func) {
+    for (let i = 0; i < array.length; i++) {
+        if (!func(array[i], i, array)) {
+            return false
+        }
+    }
+    return true 
+}
+
+function some (array, func) {
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i], i, array)) {
+            return true
+        }
+    }
+    return false 
+}
