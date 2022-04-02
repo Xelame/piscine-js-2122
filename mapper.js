cda04b0b9604807af661c523d48141c3e258dd42
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-02 15:59:14 +0200

    wouaf wouaf

--- mapper.js
+++ mapper.js
@@ -9,7 +9,11 @@ function map(array, func) {
 function flatMap (array, func) {
     const newArray = []
     for (let i = 0; i < array.length; i++) {
-        newArray.push(...func(array[i], i, array))
+        if (typeof func(array[i], i, array) === 'object') {
+            newArray.push(...func(array[i], i, array))
+        } else {
+            newArray.push(func(array[i], i, array))
+        }
     }
     return newArray
 }
