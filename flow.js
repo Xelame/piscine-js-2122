f5e4d90bf9c883d77f95abe7acf39557b3c0ec06
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-03 11:32:09 +0200

    wouaf wouaf

--- flow.js
+++ flow.js
@@ -1,5 +1,6 @@
 function flow (arrayOfFunc) {
+    const initialValue = typeof arrayOfFunc[0](arrayOfFunc[0]) === 'object' ? [] : 0
     return function (value) {
-        return arrayOfFunc.reduce((value, func) => func(value), value)
+        return arrayOfFunc.reduce((value, func) => func(value), initialValue)
     }
 }
