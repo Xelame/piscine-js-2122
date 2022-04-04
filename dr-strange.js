e934a6e6984b31f090d927b6528d09e30b6d7f2b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-04 09:21:02 +0200

    wouaf wouaf

--- dr-strange.js
+++ dr-strange.js
@@ -1,25 +1,21 @@
-function addWeek(date){
-    let day0 = new Date('0001-01-01')
-    function diffDates(day_one, day_two) {
-        return ((day_one-day_two) / (60 * 60 * 24 * 1000))/7
+function addWeek(date) {
+    let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let beginning = new Date('0001-01-01')
+    // On prend le nombre de semaine passé depuis le début de l'année 1
+    let weekNumber = Math.floor((date.getTime() - beginning.getTime()) / (7 * 24 * 60 * 60 * 1000))
+    // Si semaine impair ==> on ajoute un "second"
+    if (weekNumber % 2 === 0) {
+        return week[date.getDay()];
+    } else {
+        return "second"+week[date.getDay()];
     }
-    function getWeekDay(date) {
-        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        return days[date.getDay()];
-    }
-    let diff = diffDates(date,day0)%2
-    if(diff%2===0||diff<1){
-        return getWeekDay(date)
-    }
-    else {
-        let day = getWeekDay(date)
-        return 'second'+day
-    }
+
+     
 }
 
-function timeTravel({date,hour,minute,second}){
-   date.setHours(hour)
-   date.setMinutes(minute)
-   date.setSeconds(second)
-   return date
+function timeTravel({ date, hour, minute, second }) {
+    date.setHours(hour)
+    date.setMinutes(minute)
+    date.setSeconds(second)
+    return date
 }
