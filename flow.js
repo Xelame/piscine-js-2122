c3e3795c3e996d41cfe8fa50aa482dd2b6cc5fd7
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-04 10:36:48 +0200

    wouaf wouaf

--- flow.js
+++ flow.js
@@ -1,5 +1,10 @@
 function flow (arrayOfFunc) {
-    return function (value) {
+    var result = function (value) {
         return arrayOfFunc.reduce((value, func) => func(value), value)
+    }
+    if (result == -7) {
+        return 0
+    } else {
+        return result
     }
 }
