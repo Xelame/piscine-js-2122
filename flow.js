f803166ae6eb569915c04a75515610520302655f
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-04 10:58:21 +0200

    wouaf wouaf

--- flow.js
+++ flow.js
@@ -1,20 +1,6 @@
 function flow (arrayOfFunc) {
     return function (value) {
-        return reduceRight(arrayOfFunc, value)
-    } 
-}
-
-function reduceRight (array, func) {
-    if (typeof array[0] == 'string') {
-        return foldRight(array, func, "")
-    } else {
-        return foldRight(array, func, 0)
+        return arrayOfFunc.reverse().reduce((value, func) => func(value), 0)
     }
-}
-
-function foldRight (array, func, accumulator) {
-    for (let i = array.length - 1; i >= 0; i--) {
-        accumulator = func(accumulator, array[i], i, array)
-    }
-    return accumulator
+    
 }
