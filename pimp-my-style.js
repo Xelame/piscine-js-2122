0046bc5e3a9a537aa8a27bb53d91c8caaba0f4b4
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-05 23:45:48 +0200

    miaou

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -4,9 +4,9 @@ let index = 0
 
 export const pimp = () => {
     let button = document.getElementsByTagName('button')[0]
-    button.classList.add(styles[index] ? styles[index] : "unpimp")
+    button.classList.add(styles[index])
     index++
-    if (index >= button.classList.length) {
+    if (index >= button.classList.length - 1) {
         button.classList.remove(button.classList[button.classList.length - 2])
     }
     console.log(button.classList);
