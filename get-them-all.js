17d2d4034e758894c0eb164a7c38f55a7b43623f
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-05 09:13:09 +0200

    wouaf wouaf

--- get-them-all.js
+++ get-them-all.js
@@ -3,3 +3,21 @@ export const getArchitects = () => {
     let other = document.getElementsByTagName('span')
     return [architects, other]
 }
+
+export const getClassical = () => {
+    let classical = document.getElementsByClassName('classical')
+    let other = document.getElementsByClassName(':not(classical)')
+    return [classical, other]
+}
+
+export const getActive = () => {
+    let active = document.getSelectionByClassName('active')
+    let other = document.getSelectionByClassName(':not(active)')
+    return [active, other]
+}
+
+export const getBonannoPisano = () => {
+    let bonannoPisano = document.getElementsById('bonanno-pisano')
+    let other = document.getElementsById(':not(bonanno-pisano)')
+    return [bonannoPisano, other]
+}
