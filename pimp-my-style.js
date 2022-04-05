b14b3742bd5c9d1f171eddeff3fa32d131d20a6f
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-06 00:27:02 +0200

    miaou

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -10,7 +10,7 @@ export const pimp = () => {
     if (index == styles.length) {
         button.classList.toggle('unpimp')
     }
-    if (index != styles.length) {
+    if (index != styles.length-1) {
         index++
     }
 }
