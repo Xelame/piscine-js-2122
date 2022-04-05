44070daeed982edb1ded6370cd8e5a725aa885bb
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-05 23:44:53 +0200

    miaou

---
+++ pimp-my-style.js
@@ -0,0 +1,13 @@
+import { styles } from "./pimp-my-style.data.js"
+
+let index = 0
+
+export const pimp = () => {
+    let button = document.getElementsByTagName('button')[0]
+    button.classList.add(styles[index] ? styles[index] : "unpimp")
+    index++
+    if (index >= button.classList.length) {
+        button.classList.remove(button.classList[button.classList.length - 2])
+    }
+    console.log(button.classList);
+}
