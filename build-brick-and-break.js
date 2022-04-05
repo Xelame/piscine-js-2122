ced7db886c9786231dde1b34e0a4eb44b848c32b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-05 11:56:43 +0200

    wouaf wouaf

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -4,25 +4,25 @@ export const build = (number) => {
         let div = document.createElement('div');
         div.id = `brick-${i}`;
         div.setAttribute('foundation', i % 3 == 2);
-        document.body.appendChild(div) 
-        
+        document.body.appendChild(div)
+
         if (number === i) {
             clearInterval(intervalID)
         }
         i++
     }, 100)
+    console.log(document.querySelectorAll('brick'))
 }
 
 
 
 export const destroy = () => {
-    
+
 }
 
 export const repair = (arrayOfIds) => {
-    for (let i = 0; i < arrayOfIds.length; i++) {
-        console.log(`brick-${arrayOfIds}`)
-        const brick = document.getElementById(`brick-${arrayOfIds[i]}`)
-        brick.setAttribute('repaired', brick.foundation ? true : 'in progress')
-    }
+    console.log(arrayOfIds);
+    const brick = document.getElementById(arrayOfIds)
+    brick.setAttribute('repaired', brick.foundation ? true : 'in progress')
+
 }
