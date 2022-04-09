f3b247690a036c946e55ef641629a8b7812402e6
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 21:10:06 +0200

    wouaf wouaf

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -20,7 +20,7 @@ const reduceKeys = (object, func, acc = '') => {
     let accumulator
     for (let i in object) {
         if (!accumulator) {
-            accumulator = acc + i
+            accumulator = func(acc, i)
         } else {
             accumulator = func(accumulator, i)
         }
