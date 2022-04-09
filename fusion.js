88ad4ae987dd3feae035458ac818fa1658edec5a
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 16:48:52 +0200

    wouaf wouaf

--- fusion.js
+++ fusion.js
@@ -1,9 +1,36 @@
 const fusion = (...objects) => {
     let arrayProperties = []
+    let newObject = {}
     for (let i in objects) {
         Object.keys(objects[i]).forEach(key => arrayProperties.includes(key) ? null : arrayProperties.push(key))
     }
-    
+    arrayProperties.forEach(key => {
+        let sum
+        objects.forEach(object => {
+            sum = Join(sum, object[key])
+        })
+        newObject[key] = sum
+    })
+    console.log(newObject)
 }
 
-fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }, {d: 2, e: 3})
+const Join = (sum, newValue) => {
+    if (newValue) {
+        if (typeof sum == typeof newValue) {
+            if (typeof sum == 'number') {
+                sum += newValue
+            }
+            if (Array.isArray(sum)) {
+                sum = sum.concat(newValue)
+            }
+            if (typeof sum == 'string') {
+                sum = `${sum} ${newValue}`
+            }
+        } else {
+            sum = newValue
+        }
+    }
+    return sum
+}
+
+fusion({ a: 10, b: 8, c: "hello", e: [2] }, { a: 10, b: [2, 8] }, {c: "Alex", e: [5, 9]})
