2b79759a4a896eba3297bc96200221eea462a160
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 14:37:54 +0200

    wouaf wouaf

--- invert.js
+++ invert.js
@@ -2,7 +2,7 @@ export const invert = (object) => {
     const invert = {}
     for (let key in object) {
         console.log(typeof object[key])
-        if (typeof object[key] !== 'object') {
+        if (object[key] !== object.__proto__) {
             invert[object[key]] = key
         }
     }
