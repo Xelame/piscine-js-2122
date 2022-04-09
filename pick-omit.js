ebfb6fc1244833633c3045e32e9f79fcbbec7f75
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 15:20:37 +0200

    wouaf wouaf

--- pick-omit.js
+++ pick-omit.js
@@ -1,7 +1,7 @@
 export const pick = (object, string) => {
     let newObject = {}
     if (typeof string === 'string') {
-        object.hasOwnProperty(string) ? newObject[string] = object[string]} : null
+        object.hasOwnProperty(string) ? newObject[string] = object[string] : null
     } else {
         string.forEach(s => {
             object.hasOwnProperty(s) ? newObject[s] = object[s] : null
