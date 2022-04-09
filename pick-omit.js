31804ac2afe08f90daaff283c4e51e0346ed68af
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 15:15:29 +0200

    wouaf wouaf

--- pick-omit.js
+++ pick-omit.js
@@ -4,7 +4,7 @@ export const pick = (object, string) => {
         object.hasOwnProperty(string) ? newObject = {string : object[string]} : null
     } else {
         string.forEach(s => {
-            object.hasOwnProperty(string) ? newObject[string] = object[string] : null
+            object.hasOwnProperty(s) ? newObject[s] = object[s] : null
         })
     }
     return newObject
@@ -16,7 +16,7 @@ export const omit = (object = {}, string) => {
         object.hasOwnProperty(string) ? null : newObject = {string : object[string]} 
     } else {
         string.forEach(s => {
-            object.hasOwnProperty(string) ? null : newObject[string] = object[string]
+            object.hasOwnProperty(s) ? null : newObject[s] = object[s]
         })
     }
     return newObject
