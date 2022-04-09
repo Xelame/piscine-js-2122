bc04214bf03cbe388dd90417aae738cf508d2ae3
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 15:12:14 +0200

    wouaf wouaf

--- invert.js
+++ invert.js
@@ -1,7 +1,6 @@
 export const invert = (object) => {
     const invert = {}
     for (let key in object) {
-        console.log( object[key] === object.__proto__[key])
         if (object[key] !== object.__proto__[key]) {
             invert[object[key]] = key
         }
--- pick-omit.js
+++ pick-omit.js
@@ -1,1 +1,11 @@
-export const pick = (object, ...string) => {
+export const pick = (object = {}, string = []) => {
+    let newObject = {}
+    if (typeof string === 'string') {
+        object.hasOwnProperty(string) ? newObject = {string : object[string]} : null
+    } else {
+        string.forEach(s => {
+            object.hasOwnProperty(string) ? newObject[string] = object[string] : null
+        })
+    }
+
+}
