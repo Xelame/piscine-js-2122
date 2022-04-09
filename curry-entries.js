da28c3f292ce59f5248ef5e3e1be7b7e2be4e1de
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-10 00:26:23 +0200

    wouaf wouaf

--- curry-entries.js
+++ curry-entries.js
@@ -1,1 +1,32 @@
-const defaultCurry = (a) => (b) => a = b
+const defaultCurry = (a) => (b) => {
+    for (let [key, value] of Object.entries(b)) {
+        a[key] = value
+    }
+    return a
+}
+
+const mapCurry = (func) => (object) => {
+    const newMap = []
+    for (let [key, value] of Object.entries(object)) {
+        newMap.push(func([key, value]))
+    }
+    return Object.fromEntries(newMap)
+}
+
+const reduceCurry = (func) => (object) => {
+    let acc = 0
+    for (let [key, value] of Object.entries(object)) {
+            acc = func(acc, [key, value])
+    }
+    return acc
+}
+
+const filterCurry = (func) => (object) => {
+    let newObject = {}
+    for (let [key, value] of Object.entries(object)) {
+        if (func([key, value])) {
+            newObject[key] = value
+        }
+    }
+    return newObject
+}
