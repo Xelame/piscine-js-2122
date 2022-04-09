06e171b887fe914852d4b063ebb8cb3e815c02bf
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-09 13:00:41 +0200

    wouaf wouaf

--- gossip-grid.js
+++ gossip-grid.js
@@ -27,6 +27,7 @@ export const grid = () => {
     rangeWidth.type = 'range'
     rangeWidth.min = '200'
     rangeWidth.max = '800'
+    rangeWidth.value = ''
     rangeWidth.id = 'width'
     rangeWidth.addEventListener('input', () => {
         document.querySelectorAll('.gossip').forEach(gossip => {
@@ -39,6 +40,7 @@ export const grid = () => {
     rangeFontSize.type = 'range'
     rangeFontSize.min = '20'
     rangeFontSize.max = '40'
+    rangeFontSize.value = ''
     rangeFontSize.id = 'font-size'
     rangeFontSize.addEventListener('input', () => {
         document.querySelectorAll('.gossip').forEach(gossip => {
@@ -51,6 +53,7 @@ export const grid = () => {
     rangeBackground.type = 'range'
     rangeBackground.min = '20'
     rangeBackground.max = '75'
+    rangeBackground.value = ''
     rangeBackground.id = 'background'
     rangeBackground.addEventListener('input', () => {
         document.body.style.background = `hsl(0, 0%, ${rangeBackground.value}%)`
