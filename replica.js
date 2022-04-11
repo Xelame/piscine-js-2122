1a522f82b54eb4213a25a5936820bbb94342de4b
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-11 06:44:03 +0200

    miaou

--- replica.js
+++ replica.js
@@ -2,8 +2,13 @@ const replica = (...objects) => {
     let entries = []
     for (let object of objects) {
         for(let [key, value] of Object.entries(object)) {
+            if (key in Object.fromEntries(entries)) {
+                entries[entries.flat().findIndex((element) => element == key)].push(value)
+            }
             entries.push([key, value])
         }
     }
     return Object.fromEntries(entries)
 }
+
+console.log(replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }));
