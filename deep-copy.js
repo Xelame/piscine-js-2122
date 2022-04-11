4b6683d07071154ab600a0b6987a4b3e1b33be51
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-11 05:06:23 +0200

    miaou

--- deep-copy.js
+++ deep-copy.js
@@ -1,12 +1,6 @@
 const deepCopy = (list) => {
     if (Array.isArray(list)) {
-        if (list.length == 0) {
-            return []
-        }
-        if (list.length == 1) {
-            return list[0]
-        }
-        return list.slice(0,1).concat(list.slice(1))
+        return Array.from(list)
     } else {
         return Object.assign({}, list)
     }
