3dfcdb7dbde067d8812c5308e3f62695c0400a6c
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-11 05:55:15 +0200

    miaou

--- replica.js
+++ replica.js
@@ -1,1 +1,9 @@
-const replica = 'a'
+const replica = (...objects) => {
+    return Object.assign(objects[0], objects.slice(1))
+}
+
+console.log(replica(
+    {},
+    Object.freeze({ line: 'Replicants are like any other machine' }),
+    Object.freeze({ author: 'Rich' })
+  ));
