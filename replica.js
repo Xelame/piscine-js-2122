aa35ddd682a7818e009edd875bc168513ea22d45
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-12 04:56:19 +0200

    miaou

--- replica.js
+++ replica.js
@@ -1,38 +1,3 @@
-const fusion = (...objects) => {
-    let arrayProperties = []
-    let newObject = {}
-    for (let i in objects) {
-        Object.keys(objects[i]).forEach(key => arrayProperties.includes(key) ? null : arrayProperties.push(key))
-    }
-    arrayProperties.forEach(key => {
-        let sum
-        objects.forEach(object => {
-            sum = Join(sum, object[key])
-        })
-        newObject[key] = sum
-    })
-    return newObject
-}
-
-const Join = (sum, newValue) => {
-    if (newValue) {
-        if (typeof sum == typeof newValue) {
-            if (typeof sum == 'number') {
-                sum += newValue
-            }
-            if (Array.isArray(sum)) {
-                sum = sum.concat(newValue)
-            }
-            if (typeof sum == 'string') {
-                sum = `${sum} ${newValue}`
-            }
-        } else {
-            sum = newValue
-        }
-    }
-    return sum
-}
-
 const replica = (...objects) => {
     let entries = []
     for (let object of objects) {
