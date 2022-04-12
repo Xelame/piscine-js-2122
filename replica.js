7e6c1dc351bb511857a8a8eb4f3dd50c442452f2
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-12 05:33:37 +0200

    miaou

--- replica.js
+++ replica.js
@@ -2,7 +2,7 @@ const replica = (...objects) => {
     let entries = []
     for (let object of objects) {
         for(let [key, value] of Object.entries(object)) {
-            if (key in Object.fromEntries(entries) && Object.fromEntries(entries)[key] instanceof Object && !(value instanceof Array)) {
+            if (key in Object.fromEntries(entries) && Object.fromEntries(entries)[key] instanceof Object && !(value instanceof Array) && !(Object.fromEntries(entries)[key] instanceof Array)) {
                 entries[key] = Object.entries(Object.assign(Object.fromEntries(entries)[key], value))
             } else {
                 entries.push([key, value])
@@ -12,4 +12,4 @@ const replica = (...objects) => {
     return Object.fromEntries(entries)
 }
 
-console.log(replica({ a: { b: [2] } }, { a: [4] }))
+console.log(replica({ a: [1, 2, 4] }, { a: { b: [4] } }).a)
