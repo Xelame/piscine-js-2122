6a236c861f56eaceeaa1fd49f5c8e22d9b62237c
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-04-12 10:47:24 +0200

    wouaf wouaf

--- pronoun.js
+++ pronoun.js
@@ -1,19 +1,24 @@
 
-const regex = /\b(I |i |You |you |He |he |She |she |It |it |We |we |They |they )(\w+)/
+const regex = /\b(I|You|you|He|he|She|she|It|it|We|we|They|they)( (\w+)|\b)/
 
 const pronoun = (string = '') => {
     string = string.toLowerCase()
     let finalObject = {}
     let words = []
     while (string.match(regex)) {
-        let key = string.match(regex)[1].toLowerCase().slice(0, string.match(regex)[1].length-1)
+        let key = string.match(regex)[1].toLowerCase()
+        console.log(key);
         if (!finalObject.hasOwnProperty(key)) {
             finalObject[key] = { word: [], count: 0}
         }
-        words = [string.match(regex)[2]].filter(word => !regex.test(word+" a"))
+        words = [string.match(regex)[3]].filter(word => !regex.test(word+" a"))
         finalObject[key].count++
-        finalObject[key].word = finalObject[key].word.concat(words) 
+        if (word[0]) {
+            finalObject[key].word = finalObject[key].word.concat(words) 
+        }
         string = string.replace(regex, '$2')
     }
     return finalObject
 }
+
+console.log(pronoun("If he you want to buy something you have to pay you"))
